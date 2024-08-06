5f5c6e4cdf865b198cfb838e758981f081eb62d5
abhinand <abhinandc293@gmial.com>
2024-08-06 16:00:37 +0530

    second class js

---
+++ js/javascript.js
@@ -0,0 +1,25 @@
+b=parseInt(prompt(""))
+a=b+20
+
+if(a>90){
+    console.log("A+");
+}
+else if(a>80){
+    console.log("A");
+}
+else if(a>70){
+    console.log("B+");
+}
+else if(a>60){
+    console.log("B");
+}
+else if(a>50){
+    console.log("B+");
+}
+else if(a>40){
+    console.log("C");
+}else if(a>30){
+    console.log("C+");
+}else if(a<20){
+    console.log("FAIL");
+}
