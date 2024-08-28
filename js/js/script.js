941288f75318b5c233b6cb64c5f2a47cd764a42c
abhinandc <abhinandc2004@gmail.com>
2024-08-28 15:58:39 +0530

    inheritance

--- js/js/script.js
+++ js/js/script.js
@@ -1,56 +1,207 @@
 
+// class Car{
+//     constructor(fuel){
+//         this.fuel1=fuel
+//     }
+// startengin(){
+//     console.log(`${this.fuel1}`);
+// }
+
+// }
 
 
-// let arr={name:"abhi",age:20,lastname:"leoo",
-//     fullname:function(){
-//         return this.name+this.lastname
+
+// x=new Car("diesel")
+
+// x.startengin()
+
+
+
+// class Button{
+
+//     constructor(name){
+//         this.Button=document.createElement("button")
+//         this.Button.innerHTML=name
+//         document.getElementById("new").appendChild(this.Button)
+
+//     }
+//     set height(h){
+//         this.Button.style.height=h+"px"
+//     }
+//     set width(h){
+//         this.Button.style.width=h+"px"
+//     }
+
+//     get Height(){
+//         return parseInt(this.Button.style.height)
+//     }
+//     get Width(){
+//         return parseInt(this.Button.style.width)
+// }
+// }
+// btn1=new Button("click mee")
+// btn1.height=100
+// btn1.width=100
+// x=btn1.Height
+// console.log(x);
+
+
+// class Father {
+//     name="sathyanarayanan"
+//     job(){
+//         console.log("he is a farmer");
 //     }
 // }
-// console.log(arr.fullname());
 
-
-
-// let x={
-//     a:10,
-//     b:20,
-//     c:function () {
-//         console.log(this);
+// class son extends Father{
+//     name="abhi"
+//     designation(){
+//         console.log("he is a engineer");
 //     }
 // }
-// let y=x.c
-// y.call(x)
+// x= new son
 
-// y=x.c.bind(x)
-// y()
+// x.designation()
+// x.job()
 
 
-// x={
-//     a:10,b:20
+
+// class Father{
+//     constructor(name,age){
+//         this.name1=name
+//         this.age1=age
+//     }
+//     fun(){
+//         console.log(`${this.name1}  ${this.age1}`);
+
+//     }
 // }
-// y={
-//     c:function (params) {
-//         console.log(this.a+this.b);
+// class child extends Father{
+//     constructor(name,age){
+//         super(name,age)
+        
 //     }
 // }
 
-// z=y.c
-// z.call(x)
+// x=new child("abhi",20)
+
+// x.fun()
 
 
 
 
-class Car{
-    color="red"
-    wheel=2
-    fuel="petrol"
-    startenigin(){
-        console.log("started");
-    }
-    stop(){
-        console.log("stoped");
-    }
-}
+// 
 
 
-swift=new Car()
-console.log(swift.wheel);
+// class Father{
+//     height(){
+//         console.log("father is tall");
+//     }
+//     eye(){
+//         console.log("father has blue eyes");
+//     }
+//     strong(){
+//         console.log("father is so strong");
+//     }
+//     love(){
+//         console.log(" father loves everyone");
+//     }
+//     voice(){
+//         console.log("father has deep voice");
+//     }
+// }
+
+// class boy extends Father{
+//     bheight(){
+//         console.log("son is small");
+//     }
+// }
+// class girl extends Father{
+//     eyes(){
+//         console.log("has eyes like father");
+//     }
+// }
+
+// boy=new boy
+// girl=new girl
+
+// boy.bheight()
+// boy.height()
+// boy.strong()
+// boy.voice()
+
+// girl.eyes()
+// girl.eye()
+// girl.love()
+
+
+
+// class Vehicle{
+//     make="made in 1992"
+//     model="Model=MC1992"
+//     startengin(){
+//         console.log("Engin started");
+//     }
+// }
+// class Car extends Vehicle{
+//     doors="2 doors"
+//     honkHorn(){
+//         console.log("Beep beep");
+//     }
+// }
+
+// let car=new Car
+// console.log(car.make);
+// console.log(car.model);
+// car.startengin()
+// console.log(car.doors);
+// car.honkHorn()
+
+
+// class Person{
+//     constructor(name){
+//         this.name=name
+//     }
+//     intoduce(){
+//         console.log(`hello my name is ${this.name}`);
+//     }
+// }
+// class student extends Person{
+//     constructor(name,subject){
+//         super(name)
+
+//         this.subject=subject
+
+
+//     }
+//     study_method(){
+//         console.log(`iam studying ${this.subject}`);
+//     }
+// }
+
+// std=new student("abhi","js")
+// std.intoduce()
+// std.study_method()
+
+// class Employee{
+//     constructor(name,position){
+//         this.name=name
+//         this.position=position
+//     }
+//     describe(){
+//         console.log(`i am ${this.name} and i work as a ${this.position} `);
+//     }
+// }
+
+// class manager extends Employee{
+//     constructor(name,position){
+//         super(name,position)
+//         this.name=name
+//         this.position=position
+//     }
+
+
+// }
+
+// manager=new manager("abhi","Manager")
+// manager.describe()
