65b2b11a64b316f21885ec53531c911484d6085c
abhinandc <abhinandc2004@gmail.com>
2024-08-30 15:47:15 +0530

    trycatch

--- js/js/script.js
+++ js/js/script.js
@@ -1,59 +1,71 @@
-// class Bank{
-//     constructor(accoundt_no){
-//         this._balance=accoundt_no
-//     }
-//     depo(amount){
-     
-//          this._balance+=amount
 
-   
-      
-        
-//     }
-//     wid(balance){
+// trycatch
 
-//     this._balance-=balance
+// console.log("start");
+
+// // a=0
+// // c=a+b
+// // console.log(c);
+// try {
+//     a=0
+//     c=a+b
+//     console.log(c);
+// } catch  {
+//     console.log("b is not defined");
+// }
+
+// finally{
+//     console.log("final");
+// }
+
+// console.log("end");
+
+
+
+// function order(callback) {
+//     setTimeout(()=>{
+//         console.log("food ordered");
+//         callback()
+//     },2000)
+// }
+
+// function decorate(callback) {
+//     setTimeout(()=>{
+//         console.log("decorated");
+//         callback()
+//     },2000)
+// }
+
+// function arrange(callback) {
+//     setTimeout(()=>{
+//         console.log("arrange");
+//         callback()
+//     },2000)
+// }
+
+// order(()=>{
+//     decorate(()=>{
+//         arrange(()=>{
+//             console.log("all the task are completed");
+//         })
+//     })
+// })
+
+
+
+// function fetchData() {
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(() => {
+//             reject(3)
+            
+//         }, 1000);
+//     })
     
-
-//   }
-//     balan(last){
-//         return this._balance
-//     }
-// }
-// x= new Bank(0)
-
-// x.depo(1000)
-// console.log(x.balan());
-// x.wid(500)
-// console.log(x.balan());
-// // console.log(x.depo());
-
-
-
-//method overloading
-// class animal{
-//     sound(){
-//         console.log("sound...");
-//     }
 // }
 
-// class dog extends animal{
-//     sound(){
-//         console.log("dog bark");
-//     }
-// }
-// class cat extends animal{
-//     sound(){
-//         console.log("cat cry meow");
-//     }
-// }
-// x=new animal
-// x.sound()
-
-// x=new dog
-// x.sound()
-// y=new cat
-// y.sound()
-
-
-
+// fetchData().then((data)=>{
+//     console.log(data);
+// }).catch((error)=>{
+//     console.log("reject");
+//     console.log(error);
+// })
