124738c00ca81f590372006528c9549513b23cc1
abhinandc <abhinandc2004@gmail.com>
2024-08-31 15:27:47 +0530

    31/8

--- js/script.js
+++ js/script.js
@@ -1,40 +1,25 @@
 function GetData() {
-    fetch("https://jsonplaceholder.typicode.com/comments")
+    fetch("https://jsonplaceholder.typicode.com/albums")
     .then((res)=>{
         return res.json()
     })
     .then((data)=>{
         
-        abc=``
-        data.map((content)=>{
-            abc=`
-            <tr>
-            <th>POST-ID</th>
-            <th>ID</th>
-            <th>NAME</th>
-            <th>EMAIL</th>
 
-            <th>BODY</th>
-            </tr>
-           `
-        })
+        
     
         str=``
         data.map((content)=>{
             str+=`
-            <tr>
-            <td>${content.postId}</td>
-            <td>${content.id}</td>
-            <td>${content.name}</td>
-            <td>${content.email}</td>
-            <td>${content.body}</td>
-
-
-            </tr>`
+            <div class="asd"><li>${content.userId}</li>
+            <li>${content.id}</li>
+            <li>${content.title}</li>
+            </div>
+            `
         })
-        document.getElementById("body1").innerHTML=abc
+        
 
-        document.getElementById("body2").innerHTML=str
+        document.getElementById("div1").innerHTML=str
     })
     .catch((error)=>{
         alert(error)
