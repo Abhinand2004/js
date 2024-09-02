58d65432d7e537683a62061b51b1d176c1df454b
abhinandc <abhinandc2004@gmail.com>
2024-09-02 16:09:10 +0530

    todoc

--- js/script.js
+++ js/script.js
@@ -1,28 +1,45 @@
 function GetData() {
-    fetch("https://jsonplaceholder.typicode.com/photos")
+    fetch("https://jsonplaceholder.typicode.com/todos")
     .then((res)=>{
         return res.json()
     })
     .then((data)=>{
         
-
-        
+        abc=``
+        data.map((content)=>{
+            abc=`
+            <tr>
+            <th>USER_ID</th>
+            <th>ID</th>
+            <th>TITLE</th>
+            <th>STATUS</th>
+            </tr>
+           `
+        })
     
         str=``
         data.map((content)=>{
-            str+=`
-            <div class="asd">
-            <p>${content.title}</p>
-        <img src="${content.url}" alt="">
-
 
            
-            </div>
+            str+=`
+            <tr>
+            <td>${content.userId}</td>
+            <td>${content.id}</td>
+            <td>${content.title}</td>
+            <td id="color">${content.completed?"<span class='colort'>completed</span>":"<span class='colorf'>not completed</span>"}</td>
+
+            </tr>
             `
+          
         })
+        document.getElementById("div1").innerHTML=abc
+
+        document.getElementById("div2").innerHTML=str
+        
+        
         
 
-        document.getElementById("div1").innerHTML=str
+        // document.getElementById("div1").innerHTML=str
     })
     .catch((error)=>{
         alert(error)
