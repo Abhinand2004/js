3bfb39fe1d2d3305abdd77122df498ed22c57f0c
abhinandc <abhinandc2004@gmail.com>
2024-09-05 16:39:32 +0530

    first

--- js/asd.js
+++ js/asd.js
@@ -40,23 +40,76 @@
 
 
 
+// function success(){
+//     x=document.getElementById("date").value
+// const date=/^([0-2]\d|3[01])-([0]\d|1[0-2])-(\d{4})/
+// out=date.test(x)
+// str=``
+// if(out){
+//     str+=`
+//    <li> <h1>Date:${x}</h1></li>
+
+//     `
+// }else{
+//     str+=`
+//     <li> <h1>Enter date in correct format</h1></li>
+//      `
+// }
+
+// // str+=`${out==true?"<li>${x}</li>":"you enterd wrong"}`
+// document.getElementById("output").innerHTML=str
+// document.getElementById("date").value=""
+// }
+
+
+
+
+
+// function success(){
+//     x=document.getElementById("pass").value
+// const date=/^(?=[A-Za-z])(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()?.,])[!@#$%^&*()/.,A-Za-z]{8,}/
+
+// out=date.test(x)
+// str=``
+// if(out){
+//     str+=`
+//    <li> <h1>Name:${x}</h1></li>
+//    <li>success</li>
+//     `
+// }else{
+//     str+=`
+//     <li> <p>PassWord Must strt with an alphebet ,must contain Uppercase& Lowercase letters ,must need an symbol and a number ,want minimam 8 digits</p></li>
+//      `
+// }
+
+// // str+=`${out==true?"<li>${x}</li>":"you enterd wrong"}`
+// document.getElementById("output").innerHTML=str
+// document.getElementById("pass").value=""
+// }
+
+
+
+
+
 function success(){
-    x=document.getElementById("date").value
-const date=/^([0-2]\d|3[01])-([0]\d|1[0-2])-(\d{4})/
+    x=document.getElementById("email").value
+const date=/^[a-zA-Z]*[!@$%^&*(A-Za-z)][@]*[a-z].+\../
+
 out=date.test(x)
 str=``
 if(out){
     str+=`
-   <li> <h1>Date:${x}</h1></li>
+   <li> <h1>EMAIL:${x}</h1></li>
+   <li>success</li>
     `
 }else{
     str+=`
-    <li> <h1>Enter date in correct format</h1></li>
+    <li> <p>must ent with @gmail.com</p></li>
      `
 }
 
 // str+=`${out==true?"<li>${x}</li>":"you enterd wrong"}`
 document.getElementById("output").innerHTML=str
-document.getElementById("date").value=""
+document.getElementById("email").value=""
 }
 
